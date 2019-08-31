ea367fe01363cb4143448755b4f8f05e7af9c8dd
Xu Haozhe <xuhaozhe@ibike668.com>
2019-08-31 22:38:07 +0800

    practice 3

---
+++ practice3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x1 = np.arange(1, 11, 2)
+x2 = np.linspace(1, 9, 5)
+print np.add(x1, x2)
+print np.subtract(x1, x2)
+print np.multiply(x1, x2)
+print np.divide(x1, x2)
+print np.power(x1, x2)
+print np.remainder(x1, x2)
